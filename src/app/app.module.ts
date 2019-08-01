3460227c5e8cd68d3f608d2d3a3f1d017b7c2db7
Dima Scherbakov <dima.r.scherbakov@gmail.com>
2019-08-01 13:52:59 +0300

    add chart template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { ChartsModule } from 'ng2-charts';
 
 import { AppComponent } from './app.component';
 import { HistoryTableComponent } from './history-table/history-table.component';
+import { HistoryChartComponent } from './history-chart/history-chart.component';
 
 @NgModule({
-  declarations: [AppComponent, HistoryTableComponent],
-  imports: [BrowserModule, HttpClientModule],
+  declarations: [AppComponent, HistoryTableComponent, HistoryChartComponent],
+  imports: [BrowserModule, HttpClientModule, ChartsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
