dca6f341bf55c1842578895bda7e275aa8bb4762
Dima Scherbakov <dima.r.scherbakov@gmail.com>
2019-08-01 12:13:20 +0300

    set data from response to the table

--- src/app/history-table/history-table.component.ts
+++ src/app/history-table/history-table.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { GetHistoryServiceService } from '../get-history-service.service';
-import { HistoryRequest } from '../history-request';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-history-table',
@@ -8,11 +8,37 @@ import { HistoryRequest } from '../history-request';
   styleUrls: ['./history-table.component.css']
 })
 export class HistoryTableComponent implements OnInit {
+  rates = [];
+
   constructor(private getHistoryServiceService: GetHistoryServiceService) {}
 
   ngOnInit() {
-    this.getHistoryServiceService.getCurrencyHistory().subscribe(resp => {
-      console.log(resp);
-    });
+    this.getHistoryServiceService
+      .getCurrencyHistory()
+      // get data from response
+      .pipe(
+        map((resp: any) => {
+          return Object.keys(resp.rates).map(date => {
+            // get name of key
+            const currencyName = Object.keys(resp.rates[date])[0];
+            // TODO: set base and symbosl from url
+            return { date: date, value: resp.rates[date][currencyName] };
+          });
+        })
+      )
+      // sort by date
+      .pipe(
+        map((resp: any) => {
+          return resp.sort((a, b) => {
+            // use new variables to calm down the linter
+            const dateA: any = new Date(a.date);
+            const dateB: any = new Date(b.date);
+            return dateA - dateB;
+          });
+        })
+      )
+      .subscribe(resp => {
+        this.rates = resp;
+      });
   }
 }
